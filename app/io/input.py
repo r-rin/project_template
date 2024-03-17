bad7fff21e89d46fa186be9585aa3940ab10b687
r-rin <rafikov.rinat.m@gmail.com>
2024-03-17 20:55:50 +0200

    Created functions without implementation in `input.py` and `output.py`

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def console_in():
+    pass
+
+
+def read_file():
+    pass
+
+
+def pandas_read_file():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+
+
+def console_out(output):
+    pass
+
+
+def write_file(data):
+    pass
