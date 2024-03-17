9ec8c1e1af578dffc15b3484514f5d97876e7b09
r-rin <rafikov.rinat.m@gmail.com>
2024-03-17 21:45:12 +0200

    Wrote a docstring for each function and implemented it

--- app/io/input.py
+++ app/io/input.py
@@ -1,12 +1,75 @@
+from os import path
+from pandas import read_csv
+
+def console_in(prompt_message):
+    '''
+    Prompts the user to input data from the console. 
+    The input is expected to be in string format.
+
+    Args:
+        prompt_message (string): message to prompt the user for input.
+
+    Returns:
+        string: data that was given from the console.
+    '''
+    input_data = input(prompt_message)
+    return input_data
 
 
-def console_in():
-    pass
+def read_file(file_path):
+    '''
+    Reads a data from a file and returns it in a string format.
+
+    Args:
+        file_path (string): path to a file to read.
+    
+    Raises:
+        FileNotFoundError: if there is no file by given path.
+        TypeError: one of the arguments has a wrong type. 
+    
+    Returns:
+        list of str: data from a file located in `file_path`.
+                     Each element in a list represents a separate line.
+    '''
+    if not isinstance(file_path, str):
+        raise TypeError("type of a file_path is not string")
+
+    if not path.exists(file_path):
+        raise FileNotFoundError(f"there is no file by given path: {file_path}")
+    
+    file_to_read = open(file_path)
+    list_of_lines = file_to_read.readlines()
+    file_to_read.close()
+
+    return list_of_lines
 
 
-def read_file():
-    pass
+def pandas_read_file(file_path):
+    '''
+    Reads a data from a .csv file and returns it in a string format.
 
+    This functions uses a 'pandas' package to read a file in `file_path`.
 
-def pandas_read_file():
-    pass
+    Raises:
+        FileNotFoundError: if there is no file by given path.
+        TypeError: if one of the arguments has a wrong type.
+        ValueError: if the file_path does not have a .csv extension.
+
+    Args:
+        file_path (string): path to a file to read.
+    
+    Returns:
+        list of str: data from a file located in `file_path`.
+                     Each element in a list represents a separate line.
+    '''
+    if not isinstance(file_path, str):
+        raise TypeError("type of a file_path is not string")
+    
+    if not path.exists(file_path):
+        raise FileNotFoundError(f"there is no file by given path: {file_path}")
+    
+    if not file_path[-4:]=='.csv':
+        raise ValueError("the file must have a .csv extension.")
+    
+    pd_file = read_csv(file_path)
+    return pd_file.to_string
--- app/io/output.py
+++ app/io/output.py
@@ -1,8 +1,30 @@
 
 
 def console_out(output):
-    pass
+    '''
+    Prints given data in a console.
+
+    Args:
+        output (any): any object or data to be printed in a console.
+
+    Examples:
+        >>> console_out("Hello World!")
+        Hello World!
+        >>> console_out([1, 2, 3, 4, 5])
+        [1, 2, 3, 4, 5]
+    '''
+    print(output)
 
 
-def write_file(data):
-    pass
+def write_file(data, file_path):
+    '''
+    Writes provided data in a file.
+
+    Args:
+        data (any): any object or data to be wrote in a file.
+        file_path (string): path to a file to write in.
+    '''
+    file_to_write = open(file_path, 'w')
+    file_to_write.write(data)
+    file_to_write.write('\n')
+    file_to_write.close()
